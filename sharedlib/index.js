b0633bafc3867eecf08ab2f82e0cf32ef4481bf9
Wade <wadehuang36@gmail.com>
2017-06-03 17:47:28 -0400

    add comment

--- sharedlib/index.js
+++ sharedlib/index.js
@@ -6,7 +6,7 @@ var lib = function () {
 };
 
 lib.version = function () {
-    return "1";
+    return "1"; //change this value and re-run project1 and project2 to ensure that this lib get updated.
 };
 
 module.exports = lib;
