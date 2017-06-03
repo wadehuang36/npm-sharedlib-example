08331fae0d071974ab7b9cb8dd457a993270593e
Wade <wadehuang36@gmail.com>
2017-06-03 17:25:04 -0400

    add init

---
+++ project2/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+var lib = require("sharedlib");
+
+console.log("the version of sharedlib is",lib.version());
---
+++ sharedlib/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var lib = function () {
+    // do something
+    return undefined;
+};
+
+lib.version = function () {
+    return "1";
+};
+
+module.exports = lib;
