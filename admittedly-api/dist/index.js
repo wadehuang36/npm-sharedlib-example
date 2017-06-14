817a83b3844d815f125eb293bf108d645ca2a887
Wade <wadehuang36@gmail.com>
2017-06-14 00:07:32 -0400

    delete dist files

--- admittedly-api/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const admittedly_lib_1 = require("admittedly-lib");
-console.log(admittedly_lib_1.test1());
-//# sourceMappingURL=index.js.map
